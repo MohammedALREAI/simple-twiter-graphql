45670d4f50c973089a331b147b933ab9d159922a
unknown <mhamad.aa1997.aa@gmail.com>
2020-06-09 10:18:47 +0300

    setup the server

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const { ApolloServer } = require("apollo-server-express")
+
+const typeDefs = require("./api/Types")
+const resolvers = require("./api/Resolvers")
+
+const app = express()
+const port = process.env.PORT || 5000
+const schema={
+typeDefs,
+	resolvers
+}
+
+const server = new ApolloServer({
+     schema
+})
+
+server.applyMiddleware({ app })
+
+app.listen(port, () =>
+	console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
+)
